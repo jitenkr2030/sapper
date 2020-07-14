d14509f1e734287bc9bf005581fc14b3f8492342
jitenkr2030 <65890471+jitenkr2030@users.noreply.github.com>
2020-07-14 18:10:28 +0530

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,7 +36,7 @@
 	<title>jiten project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Great success achive by you only you can !</h1>
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
